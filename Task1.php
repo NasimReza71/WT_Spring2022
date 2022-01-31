8e2c729dd7880e8e40dba635f211506bf24a05ed
NasimReza71 <88015066+NasimReza71@users.noreply.github.com>
2022-01-31 17:08:36 +0600

    Add files via upload

---
+++ Task1.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head><title>Registration Form</title>
+</head>
+<body>
+    <h3>Registration Form</h3><br><hr>
+<form >
+
+<table>
+<tr><td>First Name:</td><td><input type="text" name="name"></td></tr>
+<tr><td>Last Name:</td><td><input type="text" name="last_name"></td></tr>
+<tr><td>Agr:</td><td><input type="number" name="number"></td></tr></table>
+<br>
+<td>
+Designation:</td>
+<input type="radio" name="Desig">Junior Programmer
+<input type="radio" name="Desig">Senior Programmer
+<input type="radio" name="Desig">Project Lead 
+<br>
+Preferred language:
+ <input type="radio"  name="language">JAVA
+<input type="radio"  name="language">PHP
+<input type="radio"  name="language">C++
+<br>
+<table>
+<tr><td>Email:</td><td><input type="text" name="name"></td></tr>
+<tr><td>Password:</td><td><input type="password" name="password"></td></tr>
+</table>
+<br>
+Please choose a file
+
+<input type="file" id="myFile" name="filename">
+  
+<br>
+<input type="submit" name="submit" value="Submit">
+<input type="submit" name="reset" value="Reset">
+
+
+
+
+
+
+
+
+
+
+
+
+
+<form>
+</body>
+</html>
